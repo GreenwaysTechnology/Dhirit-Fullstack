de18ce4c7322d4e39c0f5d00dbd3584b00d7174c
subugee <sasubramanian_md@hotmail.com>
2024-07-31 18:49:00 +0530

    ajax call

--- state-pattern/src/main.jsx
+++ state-pattern/src/main.jsx
@@ -1,45 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { produce } from 'immer';
 import './index.css'
+import { produce } from 'immer';
 
-class HouseRating extends React.Component {
-
+class Posts extends React.Component {
+    //declare state
     state = {
-        house: {
-            name: 'RavenClaw',
-            points: 10
+        posts: [], //data 
+        error: null, //error
+        isLoading: false//spinner
+    }
+
+    
+    async componentDidMount() {
+        try {
+            const url = 'https://jsonplaceholder.typicode.com/posts'
+            const response = await fetch(url);
+            const posts = await response.json()
+            //populate data with state
+            this.setState((prevState) => {
+                return produce(prevState, (draft) => {
+                    //fill api response with state
+                    draft.posts = posts
+                    draft.isLoading = true
+                })
+            })
+        }
+        catch (err) {
+            this.setState((prevState) => {
+                return produce(prevState, (draft) => {
+                    //fill api response with state
+                    draft.err = err
+                })
+            })
         }
     }
-    onIncreasePointsByTwo = () => {
-        this.setState((prveState) => {
-            // return {
-            //     ...prveState,  //level -0 copy
-            //      house: {
-            //         ...prveState.house, // level-1 copy : copy all properties within house 
-            //         points: prveState.house.points+ 2
-            //      }
-            // }
-            return produce(prveState, (draft) => {
-                //simple js way 
-                draft.house.points += 2
-            })
-        })
-    }
+
     render() {
-        return <div>
-            <h1>House Rating Component</h1>
-            <h3>House Name : {this.state.house.name}</h3>
-            <h3>Points : {this.state.house.points}</h3>
-            <button onClick={this.onIncreasePointsByTwo}>Rate</button>
-        </div>
+        //show different ui based on different state: conditional rendering
+        const { posts, error, isLoading } = this.state
+        if (error) {
+            return <div>
+                <h1>Error : {error.message}</h1>
+            </div>
+        } else if (!isLoading) {
+            return <h2>Loading...</h2>
+        } else {
+            return <div>
+                <h1>Posts</h1>
+                <hr />
+                <ul>
+                    {
+                        posts.map(post => {
+                            return <li>{post.title}</li>
+                        })
+                    }
+                </ul>
+            </div>
+        }
     }
+
 }
 
 
 const App = () => {
     return <>
-        <HouseRating />
+        <Posts />
     </>
 }
 
